c80c0643b10bfe5ec44b4eb7da63722c1c6715ba
gab <matteo.gabrieli1996@gmail.com>
2019-02-22 15:51:30 +0100

    modified os.sh

--- roles/machineInfo/files/os.sh
+++ roles/machineInfo/files/os.sh
@@ -3,6 +3,10 @@ if [ -a /etc/redhat-release ]
 then
 	cat /etc/redhat-release
 else
-	cat /etc/os-release | awk '/^PRETTY/'| awk -F'"' '{print $2}'
+	if [ -a /etc/os-release ]
+	then
+		cat /etc/os-release | awk '/^PRETTY/'| awk -F'"' '{print $2}'
+	else
+		zypper ls |grep -i sle | tail -1 | awk -F'|' '{print $2}'	
 fi
 rm -f /tmp/os.sh
